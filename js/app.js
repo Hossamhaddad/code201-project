b225a8b0e69ac1dc79895a20d636ce7bd179e81d
hossamhaddad <husamhaddad80@gmail.com>
2021-01-04 13:58:43 +0200

    adding files and codes

---
+++ js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var team = prompt('my favorite football team is RealMadrid  ? ');
+if (team.toLowerCase() === 'yes' || team.toLowerCase() === 'y') {
+  alert('who likes them ? :P ');
+} else {
+  alert('of course bacrelona is my fav');
+}
+console.log('my favorite football team is bacrelona  ');
+var food = prompt('my favorite food is Mansafff ?');
+if (food.toLowerCase() === 'yes' || food.toLowerCase() === 'y') {
+  alert('who doesnt like it !! ?? :D  ');
+} else {
+  alert('you think i like pizza !! ');
+}
+console.log('my favorite food is Mansafff  ');
+var sport = prompt('my favorite sport is swimming ? ');
+if (sport.toLowerCase() === 'yes' || sport.toLowerCase() === 'y') {
+  alert('swimming is the best sport ever');
+} else {
+  alert('its not your favorite ??!! :O');
+}
+console.log('my favorite sport is swimming ^_^  ');
+var series = prompt('my favorite series is blacklist   ? ');
+if (series.toLowerCase() === 'yes' || series.toLowerCase() === 'y') {
+  alert('i mean who doesnt like raimond ');
+} else {
+  alert('what do you suggest :P');
+}
+console.log('my favorite series is blacklist  ');
+var fruit = prompt('my favorite fruit is mango ');
+if (fruit.toLowerCase() === 'yes' || fruit.toLowerCase() === 'y') {
+  alert('im in love with mango');
+} else {
+  alert('i dont like apple :P ');
+}
+console.log('my favorite fruit is mango');
+
+
+var userName = prompt('please enter your name ^_^');
+document.write('welcome to my page ' + userName + 'i hope you like it ');
+alert('my favorite football team is bacrelona  ');
+alert('my favorite food is Mansafff  ');
+alert('my favorite sport is swimming ^_^  ');
+alert('my favorite series is blacklist  ');
+alert('my favorite fruit is mango');
+
