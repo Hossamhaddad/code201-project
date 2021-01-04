b5e9cc8c2599cf767347af01781b7342c36d037e
hossamhaddad <husamhaddad80@gmail.com>
2021-01-04 15:01:02 +0200

    commenting the console log

--- js/app.js
+++ js/app.js
@@ -2,39 +2,54 @@
 
 var team = prompt('my favorite football team is RealMadrid  ? ');
 if (team.toLowerCase() === 'yes' || team.toLowerCase() === 'y') {
-  alert('who likes them ? :P ');
+  alert('lol not a chance barcelona is the best ');
+} else if(team.toLowerCase() ==='no'|| team.toLowerCase()==='n' ) {
+  alert('correct answer ^_^');
 } else {
-  alert('of course bacrelona is my fav');
+  alert('wrong answer it should be yes,y or no, n');
+
 }
-console.log('my favorite football team is bacrelona  ');
+//console.log('my favorite football team is bacrelona  ');
 var food = prompt('my favorite food is Mansafff ?');
 if (food.toLowerCase() === 'yes' || food.toLowerCase() === 'y') {
-  alert('who doesnt like it !! ?? :D  ');
+  alert('who doesnt like it ^_^ true ');
+} else if(food.toLowerCase() ==='no'|| food.toLowerCase()==='n' ) {
+  alert('its not your favorite ??!! :O too bad wrong answer ');
 } else {
-  alert('you think i like pizza !! ');
+  alert('wrong answer it should be yes,y or no, n');
+
 }
-console.log('my favorite food is Mansafff  ');
+//console.log('my favorite food is Mansafff  ');
 var sport = prompt('my favorite sport is swimming ? ');
 if (sport.toLowerCase() === 'yes' || sport.toLowerCase() === 'y') {
-  alert('swimming is the best sport ever');
+  alert('swimming is the best sport ever correct ');
+} else if(sport.toLowerCase() ==='no'|| sport.toLowerCase()==='n' ) {
+  alert('its not your favorite ??!! bad answer:O');
 } else {
-  alert('its not your favorite ??!! :O');
+  alert('wrong answer it should be yes,y or no, n');
+
 }
-console.log('my favorite sport is swimming ^_^  ');
+//console.log('my favorite sport is swimming ^_^  ');
 var series = prompt('my favorite series is blacklist   ? ');
 if (series.toLowerCase() === 'yes' || series.toLowerCase() === 'y') {
-  alert('i mean who doesnt like raimond ');
+  alert('of course it is :D i mean who doesnt like it ?!  ');
+} else if(series.toLowerCase() ==='no'|| series.toLowerCase()==='n' ) {
+  alert('its not your favorite ??!! :O wrong answer');
 } else {
-  alert('what do you suggest :P');
+  alert('wrong answer it should be yes,y or no, n');
+
 }
-console.log('my favorite series is blacklist  ');
+//console.log('my favorite series is blacklist  ');
 var fruit = prompt('my favorite fruit is mango ');
 if (fruit.toLowerCase() === 'yes' || fruit.toLowerCase() === 'y') {
-  alert('im in love with mango');
+  alert('i really love mango :D correct');
+} else if(fruit.toLowerCase() ==='no'|| fruit.toLowerCase()==='n' ) {
+  alert('i dont like apple :P wrong answer ');
 } else {
-  alert('i dont like apple :P ');
+  alert('wrong answer it should be yes,y or no, n');
+
 }
-console.log('my favorite fruit is mango');
+//console.log('my favorite fruit is mango');
 
 
 var userName = prompt('please enter your name ^_^');
